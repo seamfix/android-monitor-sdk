9a81b0267cf60cd48ddf8a50d5349470d9528848
jeffemuveyan <jemuveyan@seamfix.com>
2020-08-22 14:34:20 +0100

    update

--- app/src/main/java/com/seamfix/appmonitortester/MainActivity.kt
+++ app/src/main/java/com/seamfix/appmonitortester/MainActivity.kt
@@ -2,11 +2,31 @@ package com.seamfix.appmonitortester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seamfix.appmonitor.login.LoginAttemptManager
+import com.seamfix.appmonitor.login.model.LoginAttempt
+import com.seamfix.appmonitor.login.model.enums.LoginMethod
+import com.seamfix.appmonitor.login.model.enums.LoginMode
+import com.seamfix.appmonitor.login.model.enums.LoginStatus
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginAttempt = LoginAttempt(0,
+            "jeffemuveyan@gmail.com",
+            100000,
+            "NA",
+            LoginStatus.SUCCESS,
+            "NA",
+            LoginMode.ONLINE,
+            LoginMethod.EMAIL)
+
+        button.setOnClickListener {
+
+        }
+
     }
 }
