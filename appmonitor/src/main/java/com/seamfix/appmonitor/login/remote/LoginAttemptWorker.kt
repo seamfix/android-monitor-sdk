dcc0f3ad815bc08a1e8d1efe4f5b7dd91f146586
jeffemuveyan <jemuveyan@seamfix.com>
2020-08-22 18:44:08 +0100

    update

--- appmonitor/src/main/java/com/seamfix/appmonitor/login/remote/LoginAttemptWorker.kt
+++ appmonitor/src/main/java/com/seamfix/appmonitor/login/remote/LoginAttemptWorker.kt
@@ -50,7 +50,7 @@ internal class LoginAttemptWorker(private val context: Context, params: WorkerPa
                     //and start the process again until the database is empty:
                     sync()
                 }else{
-                    Log.e(LoginAttemptWorker::class.java.simpleName, "Sync failed")
+                    Log.e(LoginAttemptWorker::class.java.simpleName, "Sync failed: ${response?.code()}")
                     sync()//restart
                 }
             } catch (e: Exception) {
