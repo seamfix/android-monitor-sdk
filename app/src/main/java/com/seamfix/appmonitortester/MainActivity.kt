ab5f5cbde8efb40011a8ecdeca13e5b537cbba80
jeffemuveyan <jemuveyan@seamfix.com>
2020-08-24 03:35:27 +0100

    updated: MainActivity and deleted files

--- app/src/main/java/com/seamfix/appmonitortester/MainActivity.kt
+++ app/src/main/java/com/seamfix/appmonitortester/MainActivity.kt
@@ -2,18 +2,35 @@ package com.seamfix.appmonitortester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seamfix.appmonitor.heartbeat.HeartBeat
 import com.seamfix.appmonitor.login.LoginAttemptManager
 import com.seamfix.appmonitor.login.model.LoginAttempt
 import com.seamfix.appmonitor.login.model.enums.LoginMethod
 import com.seamfix.appmonitor.login.model.enums.LoginMode
 import com.seamfix.appmonitor.login.model.enums.LoginStatus
+import com.sf.rest.request.device.DeviceHeartBeat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Start the heart beat job:
+        HeartBeat.runJob(this, 5000, object : HeartBeat.HeartbeatOperation{
+
+            override fun getDeviceHeartBeat(): com.sf.rest.request.device.DeviceHeartBeat {
+
+                //Create your device heart beat and set the values you are interested in:
+                val deviceHeartBeat = DeviceHeartBeat()
+                deviceHeartBeat.setClientCurrentTime(Date())
+
+                return deviceHeartBeat
+            }
+        })
 
         //create a login attempt object
         val loginAttempt = LoginAttempt(
